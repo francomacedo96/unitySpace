9c3effa2f629badd1e52d9750fe8c7439581d35a
francomacedo96 <francomacedo@outlook.com>
2024-08-01 14:53:52 -0300

    disparo base completo

--- Assets/Scripts/Disparo.cs
+++ Assets/Scripts/Disparo.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Disparo : MonoBehaviour
 {
     public Transform bulletSpawnPoint;
+    public Transform bulletSpawnPointTwo;
     public GameObject bulletPrefab;
     public float bulletSpeed = 10;
 
@@ -14,6 +15,9 @@ public class Disparo : MonoBehaviour
         if(Input.GetButtonDown("Fire1")) {
             var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
             bullet.GetComponent<Rigidbody2D>().velocity= bulletSpawnPoint.up * bulletSpeed;
+
+            var bulletTwo = Instantiate(bulletPrefab, bulletSpawnPointTwo.position, bulletSpawnPointTwo.rotation);
+            bulletTwo.GetComponent<Rigidbody2D>().velocity= bulletSpawnPointTwo.up * bulletSpeed;
         }
     }
 
